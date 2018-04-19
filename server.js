ea946825f0f71821161bdadac86ddc34b86f119e
Elijah Seigel <seigel180@gmail.com>
2018-04-19 14:33:05 -0500

    basic server client implemented, design documentation added

---
+++ server.js
@@ -0,0 +1,28 @@
+//standard express server taken from https://hackernoon.com/how-to-build-a-multiplayer-browser-game-4a793818c29b
+// Dependencies
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+app.set('port', 5000);
+app.use('/static', express.static(__dirname + '/static'));
+// Routing
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, 'index.html'));
+});
+// Starts the server.
+server.listen(5000, function() {
+  console.log('Starting server on port 5000');
+});
+//web socket handlers
+io.on('connection', function (socket){
+	
+});
+/* testing connection*
+setInterval(function() {
+  io.sockets.emit('message', 'hi!');
+}, 1000);
+*/
