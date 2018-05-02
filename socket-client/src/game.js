aaa0efcf3cf3b8fe6033b4108937444bde367c63
Elijah Seigel <seigel180@gmail.com>
2018-05-02 12:23:33 -0500

    tweaking package.json

--- socket-client/src/game.js
+++ socket-client/src/game.js
@@ -23,7 +23,7 @@ export default class Game{
 		this.state = 0;
 		
 		//communication endpoint
-		this.socket = socketIOClient("localhost:5000");
+		this.socket = socketIOClient("https://silent-dance-off.herokuapp.com/:5000");
 		//the room ID server side
 		this.roomID;
 		
