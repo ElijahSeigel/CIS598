ce1e5923646eb92e4258d744a4cabfdcb6784500
Elijah Seigel <seigel180@gmail.com>
2018-05-02 14:00:00 -0500

    preparing master for deployment

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ server.listen(port, function() {
 //web socket handlers
 io.on('connection', function (socket){
   console.log('User connected')
+  console.log(socket)
 
   socket.on('new_room', (name)=>{
 	  console.log('new_room')
