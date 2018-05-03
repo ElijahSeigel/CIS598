5316bd636a68cce4241386aab708e83b5cd84354
Elijah Seigel <seigel180@gmail.com>
2018-05-03 15:12:37 -0500

    preparing master for deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
-onst PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
